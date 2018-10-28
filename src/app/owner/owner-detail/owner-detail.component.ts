7fe624faafa94903e0050ec03aa99e166cc8ede3
bplindgren <bplindgren91@gmail.com>
2018-10-27 22:44:02 -0500

    stat-card-grid-list created

--- src/app/owner/owner-detail/owner-detail.component.ts
+++ src/app/owner/owner-detail/owner-detail.component.ts
@@ -4,7 +4,6 @@ import { OwnerService } from '../owner-service/owner.service';
 import { TeamService } from '../../team/team-service/team.service';
 import { ActivatedRoute } from '@angular/router';
 import { Owner } from '../../owner';
-import { AllTimeStats } from '../../allTimeStats';
 import { StatCardComponent } from '../stat-card/stat-card';
 import { MatGridListModule } from '@angular/material/grid-list';
 
@@ -15,62 +14,27 @@ import { MatGridListModule } from '@angular/material/grid-list';
 })
 export class OwnerDetailComponent implements OnInit {
   private owner: Owner;
-  private allTimeStats: AllTimeStats;
-  private cardStats: Array<String> = ["Wins", "Losses", "Ties", "Winning_Percentage", "Points_For", "Points_Against", "Point_Differential", "Points_For_Per_Game", "Points_Against_Per_Game", "PPG_Diff"]
-  private statValues: Object;
-  private objectKeys = Object.keys;
-  @Output() evtEmitterStat: EventEmitter<string> = new EventEmitter();
 
   constructor(
     private ownerService: OwnerService,
-    private teamService: TeamService,
     private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    let id = Number(this.route.params.value["id"]);
-    this.getData(id)
+    this.ownerService.getOwner(this.route.params.value["id"])
+      .subscribe(owner => {
+        this.owner = owner;
+      }
+    )
     this.route.url.subscribe(url => {
-      this.getData(url[1]);
+      this.ownerService.getOwner(url[1])
+        .subscribe(newOwner => {
+          this.owner = newOwner;
+        })
     })
   }
-
-  getData(id: number): Observable<any[]> {
-    let ownerResponse = this.ownerService.getOwner(id);
-    let statsResponse = this.ownerService.getOwnerAllTimeStats(id);
-    let teamResponse = this.teamService.getOwnerTeams(id);
-    forkJoin([ownerResponse, statsResponse, teamResponse]).subscribe(responseList => {
-      this.owner = responseList[0];
-      this.allTimeStats = responseList[1];
-      this.statValues = this.getCardStats(responseList[1]);
-    })
-  }
-
-  getCardStats(allTimeStats: AllTimeStats): Object {
-  console.log(allTimeStats)
-    let cardStats : Object = {
-      Wins: allTimeStats["wins"],
-      Losses: allTimeStats["losses"],
-      Ties: allTimeStats["ties"],
-      Winning_Percentage: allTimeStats["winningPct"],
-      Points_For: allTimeStats["pointsFor"],
-      Points_Against: allTimeStats["pointsAgainst"],
-      Point_Differential: allTimeStats["pointDifferential"],
-      Points_For_Per_Game: allTimeStats["pfpg"],
-      Points_Against_Per_Game: allTimeStats["papg"],
-      PPG_Diff: (allTimeStats["pfpg"]-allTimeStats["papg"]).toFixed(1)
-    }
-    console.log(cardStats);
-    return cardStats;
-  }
-
-  formatKeys(str: string): string {
-    return str.split("_").join(" ").split(" Percentage").join("%").split(" Per ").join("/");
-  }
-
-  getStatData(stat: string): Array<Any> {
-    this.evtEmitterStat.emit(stat);
-    console.log(stat);
+  log(str: string) {
+    console.log("NEW STAT GRAPH: " + str);
   }
 
   ngAfterViewInit() {
--- src/app/owner/stat-card-grid-list/stat-card-grid-list.component.ts
+++ src/app/owner/stat-card-grid-list/stat-card-grid-list.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter,
+  OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
+import { Observable, forkJoin } from 'rxjs';
+import { OwnerService } from '../owner-service/owner.service';
+import { TeamService } from '../../team/team-service/team.service';
+import { ActivatedRoute } from '@angular/router';
+import { Owner } from '../../owner';
+import { AllTimeStats } from '../../allTimeStats';
+import { StatCardComponent } from '../stat-card/stat-card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
-  selector: 'app-stat-card-grid-list',
+  selector: 'stat-card-grid-list',
   templateUrl: './stat-card-grid-list.component.html',
   styleUrls: ['./stat-card-grid-list.component.css']
 })
 export class StatCardGridListComponent implements OnInit {
+  @Input() owner: Owner;
+  private allTimeStats: AllTimeStats;
+  private cardStats: Array<String> = ["Wins", "Losses", "Ties", "Winning_Percentage", "Points_For", "Points_Against", "Point_Differential", "Points_For_Per_Game", "Points_Against_Per_Game", "PPG_Diff"]
+  @Output() evtEmitterStat: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private ownerService: OwnerService,
+    private teamService: TeamService,
+    private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    let id = Number(this.route.params.value["id"]);
+    this.getData(id)
+  }
+
+  getData(id: number): Observable<any[]> {
+    let statsResponse = this.ownerService.getOwnerAllTimeStats(id);
+    let teamResponse = this.teamService.getOwnerTeams(id);
+    forkJoin([statsResponse, teamResponse]).subscribe(responseList => {
+      // this.owner = responseList[0];
+      this.allTimeStats = responseList[0];
+      this.statValues = this.getCardStats(responseList[0]);
+    })
+  }
+
+  getCardStats(allTimeStats: AllTimeStats): Object {
+  console.log(allTimeStats)
+    let cardStats : Object = {
+      Wins: allTimeStats["wins"],
+      Losses: allTimeStats["losses"],
+      Ties: allTimeStats["ties"],
+      Winning_Percentage: allTimeStats["winningPct"],
+      Points_For: allTimeStats["pointsFor"],
+      Points_Against: allTimeStats["pointsAgainst"],
+      Point_Differential: allTimeStats["pointDifferential"],
+      Points_For_Per_Game: allTimeStats["pfpg"],
+      Points_Against_Per_Game: allTimeStats["papg"],
+      PPG_Diff: (allTimeStats["pfpg"]-allTimeStats["papg"]).toFixed(1)
+    }
+    console.log(cardStats);
+    return cardStats;
+  }
+
+  formatKeys(str: string): string {
+    return str.split("_").join(" ").split(" Percentage").join("%").split(" Per ").join("/");
+  }
+
+  getStatData(stat: string): void {
+    this.evtEmitterStat.emit(stat);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    let newOwnerId = changes.owner.currentValue.id;
+    this.getData(newOwnerId);
   }
 
 }
