ba338e5f57471b39ae9b3968629f7c01e57b234a
bplindgren <bplindgren91@gmail.com>
2018-11-05 22:20:24 -0600

    refactor commit

---
+++ src/app/seasons.ts
@@ -0,0 +1,34 @@
+export var seasons = [
+  {
+    "year": "2011",
+    "seasonNumber": 1
+  },
+  {
+    "year": "2012",
+    "seasonNumber": 2
+  },
+  {
+    "year": "2013",
+    "seasonNumber": 3
+  },
+  {
+    "year": "2014",
+    "seasonNumber": 4
+  },
+  {
+    "year": "2015",
+    "seasonNumber": 5
+  },
+  {
+    "year": "2016",
+    "seasonNumber": 6
+  },
+  {
+    "year": "2017",
+    "seasonNumber": 7
+  },
+  {
+    "year": "2018",
+    "seasonNumber": 8
+  }
+]
