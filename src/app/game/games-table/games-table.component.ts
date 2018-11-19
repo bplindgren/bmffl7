918b04a03f6994df13c6943ea31a496dcc8ff6da
bplindgren <bplindgren91@gmail.com>
2018-11-18 23:13:03 -0600

    teams Detail

---
+++ src/app/game/games-table/games-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, AfterViewInit, Input } from '@angular/core';
+import { Game } from '../../game';
+
+@Component({
+  selector: 'games-table',
+  templateUrl: './games-table.component.html',
+  styleUrls: ['./games-table.component.css']
+})
+export class GamesTableComponent implements AfterViewInit {
+  @Input() games: Game[];
+  @Input() datasource;
+  displayedColumns: string[] = ['week', 'gametype', 'awayteam', 'hometeam', 'awayscore', 'homescore'];
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    console.log('games table created', this.games);
+  }
+
+}
