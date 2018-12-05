d215bb3be742cef343acc0ff57d474fc8094bed0
bplindgren <bplindgren91@gmail.com>
2018-12-04 20:55:27 -0600

    teams-table solid

--- src/app/team/teams-table/teams-table.component.ts
+++ src/app/team/teams-table/teams-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import { MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { SeasonStats } from '../../seasonStats';
@@ -13,7 +13,7 @@ export class TeamsTableComponent implements OnInit, OnChanges {
   @Input() teams: SeasonStats[];
   dataSource: any;
   displayedColumns: string[] = ['year', 'name', 'standing', 'divisionwinner', 'gamesplayed', 'wins', 'losses', 'ties', 'winningpct', 'pointsfor', 'pointsagainst', 'pfpg', 'papg', 'ppgdiff'];
-  tenTeams: boolean = true;
+  public hideTeams: boolean = true;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -22,17 +22,21 @@ export class TeamsTableComponent implements OnInit, OnChanges {
     this.dataSource = new MatTableDataSource<SeasonStats>(this.teams);
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-    // this.tenTeams = this.teams.length > 10;
+    this.hideTeams = this.setTenTeams();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     this.dataSource = new MatTableDataSource<SeasonStats>(changes['teams']['currentValue']);
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-    // this.tenTeams = this.teams.length > 10;
+    this.hideTeams = this.setTenTeams();
   }
 
-  // updateTenTeams(): boolean {
-  //   return
-  // }
+  setTenTeams(): boolean {
+    return this.dataSource.data.length > 10;
+  }
+
+  ngAfterViewInit() {
+    this.hideTeams = this.dataSource.data.length > 10;
+  }
 }
