26aecfa8fd29bd4db26313449fcc45c497659790
bplindgren <bplindgren91@gmail.com>
2018-12-17 22:36:54 -0600

    vertical-bar-chart passing

---
+++ src/app/test-objects/AllTimeStatsTestObj.ts
@@ -0,0 +1,17 @@
+import { AllTimeStats } from '../allTimeStats';
+export const AllTimeStatsTestObj: AllTimeStats = {
+  "ownerId":4,
+  "firstname":"Brad",
+  "lastinitial":"L",
+  "seasons":8,
+  "wins":58,
+  "losses":65,
+  "ties":0,
+  "winningpct":47.2,
+  "pointsfor":10755.7,
+  "pointsagainst":11053.6,
+  "pointdifferential":-297.8994,
+  "pfpg":87.4,
+  "papg":89.9,
+  "ppgdiff":-2.5
+}
